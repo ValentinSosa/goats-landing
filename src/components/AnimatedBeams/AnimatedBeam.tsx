c9d972f619d115f2a2c08193bc48e66c6f5f7253
rocoSittoni <roco.sittoni@protonmail.com>
2025-06-19 16:02:32 -0300

    feat(styles): bugfixes

--- src/components/AnimatedBeams/AnimatedBeam.tsx
+++ src/components/AnimatedBeams/AnimatedBeam.tsx
@@ -97,9 +97,9 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
     // Initialize ResizeObserver
     const resizeObserver = new ResizeObserver((entries) => {
       // For all entries, recalculate the path
-      for (const entry of entries) {
+      entries.forEach(() => {
         updatePath()
-      }
+      })
     })
 
     // Observe the container element
