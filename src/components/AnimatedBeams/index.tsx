6214ca1eb2ec0276cbc00e98d0a68c92b9b4c311
sziinocolanino <sziinocolanino>
2025-06-20 12:01:36 -0300

    fix: zoom tech and update icons

--- src/components/AnimatedBeams/index.tsx
+++ src/components/AnimatedBeams/index.tsx
@@ -12,7 +12,7 @@ const Circle = forwardRef<HTMLDivElement, { className?: string; children?: React
       <div
         ref={ref}
         className={cn(
-          "z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
+          "z-10 flex size-16 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
           className,
         )}
       >
@@ -35,8 +35,8 @@ export default function AnimatedBeamDemo() {
   const div7Ref = useRef<HTMLDivElement>(null)
 
   return (
-    <div className="relative flex h-[300px] w-full items-center justify-center overflow-hidden p-10" ref={containerRef}>
-      <div className="flex size-full max-h-[200px] max-w-lg flex-col items-stretch justify-between gap-10">
+    <div className="relative flex h-[600px] w-full items-center justify-center overflow-hidden p-10" ref={containerRef}>
+      <div className="flex size-full max-h-[500px] max-w-4xl flex-col items-stretch justify-between gap-16">
         <div className="flex flex-row items-center justify-between">
           <Circle ref={div1Ref}>
             <img
@@ -47,8 +47,8 @@ export default function AnimatedBeamDemo() {
           </Circle>
           <Circle ref={div5Ref}>
              <img
-              src="https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg"
-              alt="React"
+              src="https://www.svgrepo.com/show/448260/amazon-ecs.svg"
+              alt="ECS"
               className="h-126 mx-auto"
             />  
           </Circle>
@@ -57,21 +57,21 @@ export default function AnimatedBeamDemo() {
           <Circle ref={div2Ref}>
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/640px-Postgresql_elephant.svg.png"
-              alt="React"
+              alt="Postgresql"
               className="h-126 mx-auto"
             /> 
           </Circle>
           <Circle ref={div4Ref} className="size-16">
             <img
               src="2.svg"
-              alt="React"
+              alt="Goat"
               className="h-126 mx-auto"
             /> 
           </Circle>
           <Circle ref={div6Ref}>
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/3/39/Kubernetes_logo_without_workmark.svg"
-              alt="React"
+              alt="Kubernetes"
               className="h-126 mx-auto"
             /> 
           </Circle>
@@ -79,16 +79,16 @@ export default function AnimatedBeamDemo() {
         <div className="flex flex-row items-center justify-between">
           <Circle ref={div3Ref}>
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/Docker-svgrepo-com.svg"
-              alt="React"
-              className="h-126 mx-auto"
+              src="https://cdn.iconscout.com/icon/free/png-256/free-python-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-226051.png?f=webp&w=256"
+              alt="Python"
+              className="h-120 mx-auto"
             />
             
           </Circle>
           <Circle ref={div7Ref}>
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Amazon_Lambda_architecture_logo.svg"
-              alt="React"
+              alt="Lambda"
               className="h-126 mx-auto rounded-full p-[-2]" />
           </Circle>
         </div>
